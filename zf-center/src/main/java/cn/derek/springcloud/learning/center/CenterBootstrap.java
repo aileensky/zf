c198030f7f9850726c2d97e62696dd155bb32cdd
aileensky <aileensky@foxmail.com>
2017-10-31 15:46:15 +0800

    zf-center

---
+++ zf-center/src/main/java/cn/derek/springcloud/learning/center/CenterBootstrap.java
@@ -0,0 +1,13 @@
+package cn.derek.springcloud.learning.center;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+@SpringBootApplication
+@EnableEurekaServer
+public class CenterBootstrap {
+    public static void main(String[] args) {
+        SpringApplication.run(CenterBootstrap.class, args);
+    }
+}
